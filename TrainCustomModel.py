bf512be917362cac3c138a5341fdce9ec3e66b4c
gwayne9955 <gwayne9955@yahoo.com>
2019-10-19 19:09:51 -0700

    Working model, but not berry good yet

---
+++ TrainCustomModel.py
@@ -0,0 +1,7 @@
+from imageai.Detection.Custom import DetectionModelTrainer
+
+trainer = DetectionModelTrainer()
+trainer.setModelTypeAsYOLOv3()
+trainer.setDataDirectory(data_directory="Strawberries")
+trainer.setTrainConfig(object_names_array=["red strawberry", "green strawberry"], batch_size=4, num_experiments=100, train_from_pretrained_model="pretrained-yolov3.h5")
+trainer.trainModel()
