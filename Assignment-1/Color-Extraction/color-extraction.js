f8df1e77ce0fdb63f382655003785a7dd4d4bceb
Garrett Wayne <gwayne9955@yahoo.com>
2019-11-29 11:44:13 -0800

    Update color-extraction.js

--- Assignment-1/Color-Extraction/color-extraction.js
+++ Assignment-1/Color-Extraction/color-extraction.js
@@ -1,13 +1,22 @@
-const { resolve } = require('path');
 const ColorThief = require('colorthief');
 
-const img = resolve(process.cwd(), '3F.jpg');
+const img = resolve(process.cwd(), 'rainbow.png');
 
 ColorThief.getColor(img)
-    .then(color => { console.log(color) })
+    .then(color => {
+      console.log(color);
+      var red = color[0];
+      var green = color[1];
+      var blue = color[2];
+      if (red > 105 && green < 70) {
+         console.log("The strawberry is ripe");
+      }
+      else {
+         console.log("The strawberry is not ripe");
+      }
+   })
     .catch(err => { console.log(err) })
 
-ColorThief.getPalette(img, 5)
-    .then(palette => { console.log(palette) })
-    .catch(err => { console.log(err) })
-    
+// ColorThief.getPalette(img, 5)
+//     .then(palette => { console.log(palette) })
+//     .catch(err => { console.log(err) })
